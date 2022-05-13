a5da9ba04983ab6c4c9c5d8b02a382064ccb097f
lashdid19 <lashdid19@gmail.com>
2022-05-13 19:33:42 +0700

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Global } from '@mantine/core';
+import { createGetInitialProps } from '@mantine/next';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+const getInitialProps = createGetInitialProps();
+
+
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <link href="https://fonts.googleapis.com/css2?family=Hubballi&display=swap" rel="stylesheet"/> 
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
