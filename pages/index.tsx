08956419f2671dbfe1a2ffb7549e6d9695ebecb4
lashdid19 <lashdid19@gmail.com>
2022-05-31 07:51:57 +0700

    add serverless api

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,116 +16,45 @@ import {
   List,
 } from "@mantine/core";
 import { useWindowScroll } from "@mantine/hooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ArrowUp, Send, X } from "tabler-icons-react";
-import Layout from "../layout/Layout";
 import Panel from "../parts/Panel";
 import Post from "../parts/Post";
 import Reply from "../parts/Reply";
 
+async function getPosts(){
+  const req = await fetch('http://localhost:3000/api/hello')
+  return await req.json()
+}
+
 export default function Home() {
+  type PostProps = {
+    id: number,
+    author: string,
+    title: string,
+    content:
+      string,
+    liked: boolean,
+    disliked: boolean,
+    likeCount: number,
+    dislikeCount: number,
+    replies: [{
+      id: number,
+      replier: string,
+      text: string
+    }],
+  }
+  
+  const [posts, setPosts] = useState<PostProps[]>([]);
+
+  useEffect(() => {
+    getPosts().then((res) => {
+      setPosts(res)
+    })
+  }, [])
+
   const [scroll, scrollTo] = useWindowScroll();
 
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      author: "ShrekTheThird",
-      title: "Why am i lonely?",
-      content:
-        "There is nothing to hide in here. Is it bad? Is it good? Nobody knows the answer. The system of this universe makes us felt something that lead to depressed.",
-      liked: false,
-      disliked: false,
-      likeCount: 0,
-      dislikeCount: 10,
-      replies: [],
-    },
-    {
-      id: 2,
-      author: "Kicker45",
-      title: "Do you guys knows about time relativity?",
-      content: "I'm just confused, i mean what do you think about that?",
-      liked: false,
-      disliked: false,
-      likeCount: 5,
-      dislikeCount: 1,
-      replies: [
-        {
-          id: 1,
-          replier: "Boomer123",
-          text: "This is hard",
-        },
-        {
-          id: 2,
-          replier: "TheNeighborsKid",
-          text: "Well, go ask albert einstein",
-        },
-      ],
-    },
-    {
-      id: 3,
-      author: "RickMaster69",
-      title: "Is there someone who never gives up?",
-      content:
-        "I never knew someone who never gives up, and someone who never let anybody down, also someone who never running around, and dessert anybody. Can someone find this person?",
-      liked: false,
-      disliked: false,
-      likeCount: 20,
-      dislikeCount: 0,
-      replies: [
-        {
-          id: 1,
-          replier: "Kicker45",
-          text: "LMAO, its rickroll",
-        },
-        {
-          id: 2,
-          replier: "Bot#123",
-          text: "This is deep",
-        },
-      ],
-    },
-    {
-      id: 4,
-      author: "PickleRickNoob",
-      title: "What is the funniest thing you've ever seen?",
-      liked: false,
-      disliked: false,
-      likeCount: 7,
-      dislikeCount: 1,
-      replies: [
-        {
-          id: 1,
-          replier: "Gamer69",
-          text: "Pickle rick, this is an old meme",
-        },
-        {
-          id: 2,
-          replier: "LolXD",
-          text: "IM PICKLE RIIIICK",
-        },
-        {
-          id: 3,
-          replier: "Idkwhy",
-          text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit maxime, recusandae quidem vel nobis in enim cumque laudantium ab blanditiis!",
-        },
-        {
-          id: 4,
-          replier: "Idkwhy",
-          text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit maxime, recusandae quidem vel nobis in enim cumque laudantium ab blanditiis!",
-        },
-        {
-          id: 5,
-          replier: "Idkwhy",
-          text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit maxime, recusandae quidem vel nobis in enim cumque laudantium ab blanditiis!",
-        },
-        {
-          id: 6,
-          replier: "Idkwhy",
-          text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit maxime, recusandae quidem vel nobis in enim cumque laudantium ab blanditiis!",
-        },
-      ],
-    },
-  ]);
 
   const [postId, setPostId] = useState(1);
   const [replyOpened, setReplyOpened] = useState(false);
