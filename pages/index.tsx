c3f5f50e51f290999395399c82f5d2e057f47c22
lashdid19 <lashdid19@gmail.com>
2022-06-06 11:39:57 +0700

    refactor code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,9 @@
-import {
-  Text,
-  Container,
-  Grid,
-  Stack,
-  Title,
-  Affix,
-  Transition,
-  Button,
-  Modal,
-  Input,
-  Group,
-  ActionIcon,
-  MediaQuery,
-  Divider,
-  List,
-} from "@mantine/core";
-import { useForceUpdate, useSetState, useWindowScroll } from "@mantine/hooks";
+import { Text, Container, Grid, Stack } from "@mantine/core";
 import { useCallback, useEffect, useState } from "react";
-import { ArrowUp, Send, X } from "tabler-icons-react";
-import Panel from "../parts/Panel";
-import Post from "../parts/Post";
-import Reply from "../parts/Reply";
+import Post from "../parts/home/Post";
+import ReplyModal from "../parts/home/ReplyModal";
+import Intro from "../parts/home/Intro";
+import GoUpButton from "../parts/home/GoUpButton";
 
 async function getPosts() {
   const endpoint = process.env.API_ENDPOINT;
@@ -28,40 +11,40 @@ async function getPosts() {
   return await req.json();
 }
 
-async function addReply(id: number, data: any) {
-  const endpoint = process.env.API_ENDPOINT;
-  Promise.all([
-    fetch(`${endpoint}/api/post/replies?id=${id}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }),
-    fetch(`${endpoint}/api/posts`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        change: "reply-counter",
-        id: id,
-      }),
-    }),
-  ]);
-}
+// async function addReply(id: number, data: any) {
+//   const endpoint = process.env.API_ENDPOINT;
+//   Promise.all([
+//     fetch(`${endpoint}/api/post/replies?id=${id}`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(data),
+//     }),
+//     fetch(`${endpoint}/api/posts`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         change: "reply-counter",
+//         id: id,
+//       }),
+//     }),
+//   ]);
+// }
 
 export default function Home() {
   interface PostProps {
-    id: number;
+    _id: number;
     author: string;
     title: string;
     content: string;
     liked: boolean;
     disliked: boolean;
-    likeCount: number;
-    dislikeCount: number;
-    replyCount: number;
+    like_count: number;
+    dislike_count: number;
+    reply_count: number;
     replies: [
       {
         id: number;
@@ -71,7 +54,6 @@ export default function Home() {
     ];
   }
 
-  
   const [posts, setPosts] = useState<PostProps[]>([]);
 
   useEffect(() => {
@@ -80,34 +62,32 @@ export default function Home() {
     });
   });
 
-  const [scroll, scrollTo] = useWindowScroll();
-
   const [postId, setPostId] = useState(1);
   const [replyOpened, setReplyOpened] = useState(false);
   const [replyLoaded, setReplyLoaded] = useState(false);
   const [replies, setReplies] = useState([{ id: 1, replier: "", text: "" }]);
   const [replyInput, setReplyInput] = useState("");
 
-  const getReplies = useCallback(async () => {
-    setReplyLoaded(false);
-    const endpoint = process.env.API_ENDPOINT;
-    const req = await fetch(`${endpoint}/api/post/replies?id=${postId}`);
-    const data = await req.json();
-    setReplies(data.replies);
-    setReplyLoaded(true);
-  }, [postId]);
+  // const getReplies = useCallback(async () => {
+  //   setReplyLoaded(false);
+  //   const endpoint = process.env.API_ENDPOINT;
+  //   const req = await fetch(`${endpoint}/api/post/replies?id=${postId}`);
+  //   const data = await req.json();
+  //   setReplies(data.replies);
+  //   setReplyLoaded(true);
+  // }, [postId]);
 
   //get and set reply modal by postid
-  useEffect(() => {
-    getReplies();
-  }, [getReplies]);
+  // useEffect(() => {
+  //   getReplies();
+  // }, [getReplies]);
 
   interface ReactionProps {
     id: number;
     reaction: "liked" | "disliked";
     counterReaction: "liked" | "disliked";
-    reactionCount: "likeCount" | "dislikeCount";
-    counterReactionCount: "likeCount" | "dislikeCount";
+    reactionCount: "like_count" | "dislike_count";
+    counterReactionCount: "like_count" | "dislike_count";
   }
 
   const onClickReply = (id: number) => {
@@ -118,7 +98,7 @@ export default function Home() {
   const onReact = (prop: ReactionProps) => {
     const newPosts = [...posts];
     for (let v of newPosts) {
-      if (v.id == prop.id) {
+      if (v._id == prop.id) {
         v[prop.reaction] = !v[prop.reaction];
         v[prop.reaction]
           ? (v[prop.reactionCount] += 1)
@@ -130,167 +110,71 @@ export default function Home() {
     setPosts(newPosts);
   };
 
-  const onReply = () => {
-    if (replyInput != "") {
-      addReply(postId, {
-        id: Math.floor(Math.random() * 9999),
-        replier: "DeadFace69",
-        text: replyInput,
-      });
-      getReplies();
-      setReplyInput("");
-    }
-  };
+  // const onReply = () => {
+  //   if (replyInput != "") {
+  //     addReply(postId, {
+  //       id: Math.floor(Math.random() * 9999),
+  //       replier: "DeadFace69",
+  //       text: replyInput,
+  //     });
+  //     getReplies();
+  //     setReplyInput("");
+  //   }
+  // };
 
   return (
     <>
-      <Modal
-        zIndex={250}
-        size="lg"
-        centered
-        opened={replyOpened}
-        onClose={() => setReplyOpened(false)}
-        withCloseButton={false}
-        styles={{ inner: { overflow: "hidden" } }}
-      >
-        <Grid justify="space-between" mb="sm">
-          <Title order={3} my={5}>
-            <Text
-              variant="gradient"
-              gradient={{ from: "orange", to: "red" }}
-              inherit
-              component="span"
-            >
-              Replies
-            </Text>
-          </Title>
-          <ActionIcon onClick={() => setReplyOpened(false)}>
-            <X />
-          </ActionIcon>
-        </Grid>
-        {replyLoaded ? (
-          replies.length > 0 ? (
-            <div style={{ height: "65vh", overflow: "auto" }}>
-              <Stack>
-                {replies.map((v) => (
-                  <Reply key={v.id} replier={v.replier} text={v.text} />
-                ))}
-              </Stack>
-            </div>
-          ) : (
-            <Text size="xs" component="span" style={{ color: "gray" }}>
-              No Replies
-            </Text>
-          )
-        ) : (
-          <Text size="xs" component="span" style={{ color: "gray" }}>
-            Loading...
-          </Text>
-        )}
-        <Group noWrap>
-          <Input
-            my="sm"
-            variant="default"
-            placeholder="Reply to this post"
-            style={{ width: "100%" }}
-            value={replyInput}
-            onInput={(e: any) => setReplyInput(e.target.value)}
-          />
-          <ActionIcon variant="filled" color="orange" onClick={onReply}>
-            <Send size={16} />
-          </ActionIcon>
-        </Group>
-      </Modal>
+      <ReplyModal
+        {...{
+          replies,
+          replyInput,
+          replyLoaded,
+          replyOpened,
+          setReplyInput,
+          setReplyOpened
+        }}
+      />
       <Container size="md" mb="4rem">
         <Grid grow>
           <Grid.Col span={6}>
             <Stack>
               {posts.map((post) => (
                 <Post
-                  key={post.id}
+                  key={post._id}
                   author={post.author}
                   title={post.title}
-                  likeCount={post.likeCount}
+                  likeCount={post.like_count}
                   onLiked={() =>
                     onReact({
-                      id: post.id,
+                      id: post._id,
                       reaction: "liked",
                       counterReaction: "disliked",
-                      reactionCount: "likeCount",
-                      counterReactionCount: "dislikeCount",
+                      reactionCount: "like_count",
+                      counterReactionCount: "dislike_count",
                     })
                   }
-                  dislikeCount={post.dislikeCount}
+                  dislikeCount={post.dislike_count}
                   onDisliked={() =>
                     onReact({
-                      id: post.id,
+                      id: post._id,
                       reaction: "disliked",
                       counterReaction: "liked",
-                      reactionCount: "dislikeCount",
-                      counterReactionCount: "likeCount",
+                      reactionCount: "dislike_count",
+                      counterReactionCount: "like_count",
                     })
                   }
-                  onClickReply={() => onClickReply(post.id)}
-                  replyCount={post.replyCount}
+                  onClickReply={() => onClickReply(post._id)}
+                  replyCount={post.reply_count}
                 >
                   <Text my="sm">{post.content}</Text>
                 </Post>
               ))}
             </Stack>
           </Grid.Col>
-          <MediaQuery smallerThan="md" styles={{ display: "none" }}>
-            <Grid.Col span={2}>
-              <Panel>
-                <Title order={1}>
-                  <Text
-                    variant="gradient"
-                    gradient={{ from: "orange", to: "red" }}
-                    inherit
-                    component="span"
-                  >
-                    Welcome
-                  </Text>
-                </Title>
-                <Divider my="xs" />
-                <Text>Here you can post almost anything.</Text>
-                <List>
-                  <List.Item>Poll</List.Item>
-                  <List.Item>Video</List.Item>
-                  <List.Item>Audio</List.Item>
-                  <List.Item>Discussion</List.Item>
-                  <List.Item>Just Asking</List.Item>
-                </List>
-                <Button
-                  component="a"
-                  href="submit"
-                  variant="gradient"
-                  gradient={{ from: "orange", to: "red" }}
-                  mt="sm"
-                  style={{ width: "100%" }}
-                >
-                  Add Post
-                </Button>
-              </Panel>
-            </Grid.Col>
-          </MediaQuery>
+          <Intro />
         </Grid>
       </Container>
-      <Affix position={{ bottom: 20, right: 20 }}>
-        <Transition transition="slide-up" mounted={scroll.y > 0}>
-          {(transitionStyles) => (
-            <Button
-              variant="gradient"
-              gradient={{ from: "orange", to: "red" }}
-              leftIcon={<ArrowUp />}
-              style={transitionStyles}
-              color="orange"
-              onClick={() => scrollTo({ y: 0 })}
-            >
-              Scroll to top
-            </Button>
-          )}
-        </Transition>
-      </Affix>
+      <GoUpButton/>
     </>
   );
 }
